638472a35c4a85546cf473b07ebccdbd659634c8
SergiiTernytskyi <105019200+SergiiTernytskyi@users.noreply.github.com>
2023-04-27 20:40:33 +0000

    Deploying to gh-pages from @ SergiiTernytskyi/test-tweets@f9a2c279046bbd241073769329c9814c539d0049 🚀

---
+++ static/js/266.482e8a2b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktest_tweets=self.webpackChunktest_tweets||[]).push([[266],{266:function(e,t,s){s.r(t);var n=s(184);t.default=function(){return(0,n.jsx)("div",{children:"Home"})}}}]);
+//# sourceMappingURL=266.482e8a2b.chunk.js.map
