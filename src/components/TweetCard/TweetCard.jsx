f627757df6aaff5f211ea251e3b8442c484a795a
Sergii Ternytskyi <serhij86@gmail.com>
2023-04-28 23:58:16 +0300

    Update TweetCard.jsx

--- src/components/TweetCard/TweetCard.jsx
+++ src/components/TweetCard/TweetCard.jsx
@@ -1,4 +1,4 @@
-import Button from 'components/FollowButton/FollowButton';
+import FollowButton from 'components/FollowButton/FollowButton';
 
 import heroPicture from 'images/heroPicture.png';
 import logo from 'images/Logo.svg';
@@ -15,36 +15,34 @@ import UserAvatar from 'components/UserAvatar/UserAvatar';
 import { useState } from 'react';
 import { numberConvert } from 'services/numberConvert';
 
-const TweetCard = ({ userTweets }) => {
-  // const [followers, setFollowers] = useState(100500);
+const TweetCard = () => {
+  const [followers, setFollowers] = useState(100500);
   const [activeFollow, setActiveFollow] = useState(false);
 
-  const { tweets, followers, avatar } = userTweets;
-
-  // const followersToggle = () => {
-  //   if (activeFollow) {
-  //     setFollowers(prevState => prevState - 1);
-  //     setActiveFollow(false);
-  //   } else {
-  //     setFollowers(prevState => prevState + 1);
-  //     setActiveFollow(true);
-  //   }
-  // };
+  const followersToggle = () => {
+    if (activeFollow) {
+      setFollowers(prevState => prevState - 1);
+      setActiveFollow(false);
+    } else {
+      setFollowers(prevState => prevState + 1);
+      setActiveFollow(true);
+    }
+  };
 
   return (
     <Card>
       <Logo src={logo} alt="company logo" />
       <HeroPicture src={heroPicture} alt="main" />
 
-      <UserAvatar avatar={avatar} />
+      <UserAvatar avatar={null} />
 
       <Wrapper>
         <Info>
-          <Text>{numberConvert(tweets)} Tweets</Text>
+          <Text>{numberConvert(1000)} Tweets</Text>
           <Text>{numberConvert(followers)} Followers</Text>
         </Info>
 
-        <Button activeFollow={activeFollow} />
+        <FollowButton onClick={followersToggle} activeFollow={activeFollow} />
       </Wrapper>
     </Card>
   );
